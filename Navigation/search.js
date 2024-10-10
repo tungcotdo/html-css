83fb1a45cf19ff3b0262580314c1249a1913d635
Dev Tung <tungcotdo@gmail.com>
2024-10-11 04:36:12 +0700

    update code

--- Navigation/search.js
+++ Navigation/search.js
@@ -1,45 +1,44 @@
 function GetSearchItem(Title, Content, SearchValue, MatchIndex){
     let Middle = 80;
-    
-    if( MatchIndex > Middle ){
-        Content = '...' + Content.substring(MatchIndex - Middle, MatchIndex + Middle) + '...';
-    }else{
-        Content = Content.substring(0, Middle * 2) + '...';
-    }
+
+    Content = MatchIndex > Middle 
+                ? '...' + Content.substring(MatchIndex - Middle, MatchIndex + Middle) + '...'
+                : Content = Content.substring(0, Middle * 2) + '...';
 
     Content = Content.replace(new RegExp(SearchValue, 'gi'), '<mark>$&</mark>');
-    return `<div class="NavSearchItem">
-                <p class="NavSearchTitle">${Title}</p>
-                <div class="NavSearchContent">
-                    <svg class="NavSearchContent-Icon" width="25" height="25" viewBox="0 0 20 20"><path d="M17 5H3h14zm0 5H3h14zm0 5H3h14z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linejoin="round"></path></svg>
-                    <p class="NavSearchContent-Desc">${Content}</p>
+    return `<div class="NavLgSearchItem">
+                <p class="NavLgSearchTitle">${Title}</p>
+                <div class="NavLgSearchContent">
+                    <svg class="NavLgSearchContent-Icon" width="25" height="25" viewBox="0 0 20 20"><path d="M17 5H3h14zm0 5H3h14zm0 5H3h14z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linejoin="round"></path></svg>
+                    <p class="NavLgSearchContent-Desc">${Content}</p>
                 </div>
             </div>`;
 } 
 
-document.getElementById('NavSearch').style.display = "none";
+document.getElementById('NavLgSearch').style.display = "none";
 
 fetch('https://dummyjson.com/posts')
 .then(res => res.json())
 .then(response => {
 
-    document.getElementById('NavSearch').style.display = "block";
-    const searchInput = document.getElementById('NavSearchFormInput');
+    document.getElementById('NavLgSearch').style.display = "block";
+    const searchInput = document.getElementById('NavLgSearchFormInput');
     searchInput.addEventListener("keyup", () => {
-        let NavSearchResult = document.getElementById('NavSearchResult');
+
+        let NavLgSearchResult = document.getElementById('NavLgSearchResult');
         let SearchValue = searchInput.value;
 
         if( SearchValue == '' ){
-            NavSearchResult.innerHTML = `<p class="NavSearchResult-No">No recent searches</p>`;
+            NavLgSearchResult.innerHTML = `<p class="NavLgSearchResult-No">No recent searches</p>`;
             return;
         }
 
-        NavSearchResult.innerHTML = '';
+        NavLgSearchResult.innerHTML = '';
         response.posts.forEach(Item => {
             let Title = Item.title; let Content = Item.body;
             if( Content.toUpperCase().indexOf(SearchValue.toUpperCase()) != -1 ){
                 let MatchIndex = Content.indexOf(SearchValue);
-                NavSearchResult.innerHTML += GetSearchItem(Title, Content, SearchValue, MatchIndex);
+                NavLgSearchResult.innerHTML += GetSearchItem(Title, Content, SearchValue, MatchIndex);
             }
         });
 
